6f4e39a1afb7c3d83324799caca7c790aedda315
Artis3n <dev@artis3nal.com>
2021-01-12 23:48:38 -0500

    Re-add the TS test key to trigger the error message

--- src/main.ts
+++ src/main.ts
@@ -85,7 +85,7 @@ const convertedMarkdown = convert(content, {
     codeTheme: getInput("code_theme"),
   });
 
-  const confluence = new Confluence(confluenceUrl, auth, spaceKey);
+  const confluence = new Confluence(confluenceUrl, auth, "TS");
   const page: ConfluencePage = {
     title: "Markdown Test",
     body: markup,
