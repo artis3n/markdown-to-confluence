f952d9ea03b2604013d6ddf280f558712e6a6014
Artis3n <dev@artis3nal.com>
2021-01-13 23:00:52 -0500

    Add coverage configuration to jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     clearMocks: true,
     moduleFileExtensions: ['js', 'ts'],
+    collectCoverageFrom: ['src/*.ts'],
     testEnvironment: 'node',
     testMatch: ['**/*.test.ts'],
     testRunner: 'jest-circus/runner',
